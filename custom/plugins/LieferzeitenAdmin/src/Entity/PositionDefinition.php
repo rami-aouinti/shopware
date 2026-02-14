bf161aa761ffb2a49eeffa4b5bab846c74f23176
Rami <rami.aouinti@gmail.com>
2026-02-14 18:09:11 +0100

    Add SAN6 position quantities and shipping labels in admin

--- custom/plugins/LieferzeitenAdmin/src/Entity/PositionDefinition.php
+++ custom/plugins/LieferzeitenAdmin/src/Entity/PositionDefinition.php
@@ -10,6 +10,7 @@ use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\FkField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\IntField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\LongTextField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
@@ -43,6 +44,8 @@ class PositionDefinition extends EntityDefinition
             new StringField('article_number', 'articleNumber'),
             new StringField('status', 'status'),
             new DateTimeField('ordered_at', 'orderedAt'),
+            new IntField('ordered_quantity', 'orderedQuantity'),
+            new IntField('shipped_quantity', 'shippedQuantity'),
             new LongTextField('comment', 'comment'),
             new LongTextField('current_comment', 'currentComment'),
             new DateTimeField('additional_delivery_request_at', 'additionalDeliveryRequestAt'),
